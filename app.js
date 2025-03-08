cec8353da0dc2ae8021505db7120eaf4084aa770
better_me <erikkhan2019@gmail.com>
2025-03-09 02:26:21 +0700

    fix cors

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
-app.use(cors());
+app.use(cors({ origin: "*" }))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
